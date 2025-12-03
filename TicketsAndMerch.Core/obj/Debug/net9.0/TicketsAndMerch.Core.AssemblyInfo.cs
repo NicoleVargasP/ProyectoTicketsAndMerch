2075e3664694ee62e0f262ad0ac0dcc1daceba15
Nicole Vargas <ariana.vargas.p@ucb.edu.bo>
2025-12-03 19:47:12 -0400

    Correccion de Errores

--- TicketsAndMerch.Core/obj/Debug/net9.0/TicketsAndMerch.Core.AssemblyInfo.cs
+++ TicketsAndMerch.Core/obj/Debug/net9.0/TicketsAndMerch.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketsAndMerch.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfbd09e26241dfa45767dad8b908a04b43eb233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993e326e92fb0ab28defbaef78a7cddafb5048b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketsAndMerch.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketsAndMerch.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TicketsAndMerch.Infrastructure/obj/Release/net9.0/TicketsAndMerch.Infrastructure.AssemblyInfo.cs
+++ TicketsAndMerch.Infrastructure/obj/Release/net9.0/TicketsAndMerch.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketsAndMerch.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfbd09e26241dfa45767dad8b908a04b43eb233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993e326e92fb0ab28defbaef78a7cddafb5048b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketsAndMerch.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketsAndMerch.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
